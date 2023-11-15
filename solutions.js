7d1ce9dd7a67d83092f94bca7d2f308ca7ac21d1
Jim Glover <gloverjr@gmail.com>
2023-11-15 13:49:36 -0600

    added anserws to ten functions in solutionsjs

--- solutions.js
+++ solutions.js
@@ -20,4 +20,55 @@
  * and
  * concat
  */
+// Question 1
+function  isTrue(input) {
+   return input === true;
+}
+// Question 2
+function isFalse(input) {
+    return input === false;
+}
+// Question 3
+function not(input) {
+    return !input;
+}
+// Question 4
+function addOne(input) {
+    // Convert the input to a number
+    let numericInput = Number(input);
 
+    // Check if the conversion resulted in a valid number
+    if (!isNaN(numericInput)) {
+        return numericInput + 1;
+    } else {
+        return NaN;
+    }
+}
+// Question 5
+function isEven(input) {
+    // Convert the input to a number if it's a string
+    let number = typeof input === 'string' ? Number(input) : input;
+
+    // Check if the number is an even number
+    return typeof number === 'number' && !isNaN(number) && number % 2 === 0;
+}
+// Question 6
+function isIdentical(a, b) {
+    return a === b;
+}
+// Question 7
+function isEqual(a, b) {
+    return a == b;
+}
+// Question 8
+function or(a, b) {
+    return a || b;
+}
+//Question 9
+function and(a, b) {
+    return a && b;
+}
+//Question 10
+function concat(a, b) {
+    return String(a) + String(b);
+}
